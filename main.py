cbaa4950338ff0b664d2f9cf70cfc2ca15e5603e
cyricky91 <134047916+cyricky91@users.noreply.github.com>
2026-04-10 22:59:37 +0800

    Update main.py

--- main.py
+++ main.py
@@ -20,7 +20,7 @@ chat_id = os.getenv("CHAT_ID")
 send_to_tg = True
 
 # 2. 監控參數
-stock_ids = "AAPL, NVDA, TSLA, MSFT, FORM, SKYQ, OGN" 
+stock_ids = "1138.HK" 
 time_period = "1y"
 volume_spike_threshold = 2.0
 bins_count = 70
